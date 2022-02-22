9def49be5b5081e3186178e2a6ac880d1701d2c2
Елизавета <86424489+L1s-is@users.noreply.github.com>
2022-02-22 09:02:44 +0500

    открытие навигационного меня через js

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -5,37 +5,62 @@ const formBox = document.querySelector(".form__box");
 const form = formBox.querySelector(".work__form");
 const talkButton = document.querySelector(".work__button");
 export const body = document.querySelector("body");
+const navList = document.querySelector(".nav__list");
 
 export function formOpen() {
     const closeButton = form.querySelector(".btn--close");
     const burger = document.querySelector(".menu__btn");
 
     talkButton.addEventListener("click", onClickOpenForm);
-    burger.addEventListener("click", onClickNotScroll)
+    burger.addEventListener("click", onClickOpenList)
+
+    function onClickOpenList() {
+        navList.classList.toggle("open")
+        onClickNotScroll()
+        document.addEventListener("click", onClickCloseListNotBtn)
+
+        function onClickCloseListNotBtn(evt) {
+            let target = evt.target;
+            if (target !== burger) {
+                let itsForm = target === (navList || navList.contains(target));
+                let formIsActive = navList.classList.contains("open");
+                if (!itsForm && !formIsActive) {
+                    onClickCloseList();
+                }
+            }
+        }
+
+        function onClickCloseList() {
+            removeClass(body, "not-scroll");
+            removeClass(navList, "open");
+            document.removeEventListener("click", onClickCloseListNotBtn);
+        }
+
+    }
+
 
     function onClickOpenForm() {
         removeClass(formBox, "hidden");
         addClass(body, "not-scroll");
-        //body.style.overflow = "hidden";
         document.addEventListener("click", onClickCloseFormNotBtn);
         closeButton.addEventListener("click", onClickCloseForm);
+    }
+
+    function onClickCloseFormNotBtn(evt) {
+        let target = evt.target;
+        if (target !== talkButton) {
+            let itsForm = target === form || form.contains(target);
+            let formIsActive = formBox.classList.contains("hidden");
+
+            if (!itsForm && !formIsActive) {
+                onClickCloseForm();
+            }
+        }
     }
 }
 
 export function onClickNotScroll() {
     body.classList.toggle("not-scroll");
-}
-
-function onClickCloseFormNotBtn(evt) {
-    let target = evt.target;
-    if (target !== talkButton) {
-        let itsForm = target === form || form.contains(target);
-        let formIsActive = formBox.classList.contains("hidden");
-
-        if (!itsForm && !formIsActive) {
-            onClickCloseForm();
-        }
-    }
 }
 
 export function onClickCloseForm() {
