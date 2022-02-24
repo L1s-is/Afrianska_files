945fb24474e741664b964f4579e878d1e3e279c7
Елизавета <86424489+L1s-is@users.noreply.github.com>
2022-02-24 19:25:25 +0500

    убрала инлайн стили при валидации формы

--- src/js/modules/validaton_form.js
+++ src/js/modules/validaton_form.js
@@ -11,24 +11,24 @@ export function validationForm() {
     const inputName = document.querySelector("#name");
     const inputEmail = document.querySelector("#email");
 
-    function createBorderStyle(element, color) {
-        element.style.borderColor = color;
-    }
-
     function showValidError(formElement) {
         let elementValue = formElement.value.replace(/ +/g, ' ').trim();
-        createBorderStyle(formElement, "red");
+        addClass(formElement, "element-stroke--invalid");
         if (!elementValue) {
             addClass(formElement, "form__element--error");
             showMessageValidity(formElement, "Please, fill the field");
         } else if (elementValue.length < formElement.minLength) {
+            addClass(formElement, "element-stroke--invalid");
+            removeClass(formElement, "element-stroke--valid");
             showMessageValidity(formElement, `The field can contain at least ${formElement.minLength} characters. Entered ${elementValue.length} characters.`);
         } else if (elementValue.length > formElement.maxLength) {
+            addClass(formElement, "element-stroke--invalid");
+            removeClass(formElement, "element-stroke--valid");
             showMessageValidity(formElement, `The field can contain no more than ${formElement.maxLength} characters. Entered ${elementValue.length} characters.`);
         } else {
             showMessageValidity(formElement, "");
             removeClass(formElement, "form__element--error");
-            createBorderStyle(formElement, "green");
+            addClass(formElement, "element-stroke--valid");
         }
     }
 
@@ -41,20 +41,22 @@ export function validationForm() {
     function updateInput() {
         if (!inputEmail.value) {
             showMessageValidity(inputEmail, "The field cannot be empty");
-            createBorderStyle(inputEmail, "red");
+            addClass(inputEmail, "element-stroke--invalid");
         } else if (validateEmail(inputEmail.value)) {
             showMessageValidity(inputEmail, "");
             removeClass(inputEmail, "form__element--error");
-            createBorderStyle(inputEmail, "green");
+            removeClass(inputEmail, "element-stroke--invalid");
+            addClass(inputEmail, "element-stroke--valid");
         } else {
             showMessageValidity(inputEmail, "The field must contain an entry like example@mail.com");
-            createBorderStyle(inputEmail, "red");
+            removeClass(inputEmail, "element-stroke--valid");
+            addClass(inputEmail, "element-stroke--invalid");
         }
 
     }
 
     inputEmail.addEventListener("input", updateInput);
-    inputName.addEventListener("input", showValidError);
+    inputName.addEventListener("input", () => showValidError(inputName));
 
     function submitUpdateEmailInput() {
         addClass(inputEmail, "form__element--error");
@@ -91,8 +93,10 @@ export function validationForm() {
         upLoadHandler(sendDataURL, new FormData(form), onClickCloseForm);
 
         form.reset();
-        inputName.style = "none";
-        inputEmail.style = "none";
+        removeClass(inputEmail, "element-stroke--valid");
+        removeClass(inputEmail, "element-stroke--invalid");
+        removeClass(inputName, "element-stroke--valid");
+        removeClass(inputName, "element-stroke--invalid");
         formElementValidHandler(inputName);
         formElementValidHandler(inputEmail);
         evt.preventDefault();
